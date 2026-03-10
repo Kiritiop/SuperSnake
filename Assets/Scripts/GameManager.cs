911c119b197c2ff34689ef6dc4169660f5bc9640
zvincentt <zvincentzeng@gmail.com>
2026-03-10 12:28:54 -0400

    Pause Menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
     public GameObject gameOverPanel;
+
+    public GameObject pauseMenu;
+
+    public static bool isPaused = false;
+
+    public static bool gameOver = false;
 
     void Awake()
     {
@@ -18,11 +25,42 @@ public class GameManager : MonoBehaviour
         ScoreManager.Instance.SaveHighScore();
         Time.timeScale = 0f;
         if (gameOverPanel) gameOverPanel.SetActive(true);
+        gameOver = true;
     }
 
     public void RestartGame()
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        gameOver = false;
     }
+
+    void Update()
+    {
+        if((Keyboard.current.escapeKey.wasPressedThisFrame || Keyboard.current.pKey.wasPressedThisFrame) && !gameOver)
+        {
+            if(isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }    
 }
